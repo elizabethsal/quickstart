a7058d7a3f815f2d65c4f5a86f44ed5d4e7fc0ea
elizabethsal <dasharil1997@yandex.ru>
2022-06-30 22:03:06 +0400

    Refactored triangleArea.

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -3,15 +3,13 @@ public class Triangle {
         triangle();
     }*/
 
-    public int triangle(int a, int b, int c) {
-        int half_perimeter = (a + b + c) / 2;
-        int square = (int) Math.sqrt(half_perimeter * (half_perimeter - a) * (half_perimeter - b) * (half_perimeter - c));
+    public int triangleArea(int a, int b, int c) {
         if (a < 0 || b < 0 || c < 0) {
             throw new IllegalArgumentException("You can't use negative variable");
-        } else {
-            return square;
-            //   System.out.println("Triangle square: " + square);
+
         }
+        int halfPerimeter = (a + b + c) / 2;
+        return (int) Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
     }
 }
 
