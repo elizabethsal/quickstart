7f07c5d9209c3c1e18d2950836b5d47c56a2c635
elizabethsal <dasharil1997@yandex.ru>
2022-07-07 19:08:22 +0400

    Refactored class MainTest.

--- src/test/java/lesson13_1/MainTest.java
+++ src/test/java/lesson13_1/MainTest.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class MainTest {
