48854bb12fe12d8483b5212af07f545d699a880d
Daria <darsertar@gmail.com>
2022-07-09 17:17:54 +0400

    Add annotation for create allure report.

--- src/test/java/lesson13_3/SimpleCalculatorTest.java
+++ src/test/java/lesson13_3/SimpleCalculatorTest.java
@@ -1,24 +1,31 @@
 package lesson13_3;
 
-import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
+@Epic("Simple calculator Test Epic")
+@Feature("Verify simple calculator")
 public class SimpleCalculatorTest {
 
 
     @Test
-    @DisplayName("Two plus two equals four")
+    @Story("Check simple addition operation")
+    @Description("Should check 2 + 2, return 4")
     public void twoPlusTwoEqualsFour() {
         SimpleCalculator calculator = new SimpleCalculator();
         assertTrue(calculator.add(2, 2) == 4);
     }
 
     @Test
-    @DisplayName("Four plus seven equal eleven")
+    @Story("Check simple addition operation")
+    @Description("Should check 4 + 7, return 11")
     public void fourPlusSevenEqualsEleven() {
         SimpleCalculator elevenEq = new SimpleCalculator();
         assertEquals(11, elevenEq.add(4, 7));
